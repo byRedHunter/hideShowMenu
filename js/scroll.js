d3ed9f40ccc3c1849303428e62b8547cb5ae370e
Jhonny Quispe <jhonny.quispejl@gmail.com>
2020-02-23 09:16:17 -0500

    Tutorial efecto scroll, mostrar u ocultar el menu

---
+++ js/scroll.js
@@ -0,0 +1,32 @@
+/* --pocicion inicial */
+let ubicacionPrincipal = window.pageYOffset;
+let $nav = document.querySelector("nav");
+
+/* --evento scroll */
+window.addEventListener("scroll", function() {
+    /* --muestra la ubicacion cada vez que hagas scroll */
+    //console.log(window.pageYOffset);
+
+    /* --donde nos encontramos actualmente */
+    let desplazamientoActual = window.pageYOffset;
+
+    /* --condicon para ocultar o mostrar el menu */
+    if(ubicacionPrincipal >= desplazamientoActual) {
+        /* --si es mayor o igual se muesta */
+        $nav.style.top = "0px";
+        console.log('Ubicacion Principal')
+        console.log(ubicacionPrincipal)
+        console.log('desplazamiento')
+        console.log(desplazamientoActual)
+    } else {
+        /* --sino lo ocultamos añadiendo un top negativo */
+        $nav.style.top = "-80px";
+        console.log('Ubicacion Principal')
+        console.log(ubicacionPrincipal)
+        console.log('desplazamiento')
+        console.log(desplazamientoActual)
+    }
+
+    /* --actulizamos la ubicacion principal */
+    ubicacionPrincipal = desplazamientoActual;
+});
